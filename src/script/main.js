275c1e8ad315422a00a415931c502502b9460f9b
Greg Kronmiller <kronmillerg@gmail.com>
2018-09-22 14:23:00 -0700

    Background color

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,5 @@
 // I am a comment
 Crafty.init(800, 600, document.getElementById('game'));
+Crafty.background('#007f3f');
 Crafty.e('2D, DOM, Color').attr({x: 0, y: 200, w: 800, h: 200}).color('#007f00');
 Crafty.e('2D, DOM, Color').attr({x: 300, y: 0, w: 200, h: 600}).color('#007f00');
