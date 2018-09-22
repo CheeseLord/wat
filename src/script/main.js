8b757fd0686dda460bee229db867eaf014b41ac0
Greg Kronmiller <kronmillerg@gmail.com>
2018-09-22 14:11:07 -0700

    Declare character encoding

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,4 @@
 // I am a comment
-Crafty.init(800,600, document.getElementById('game'));
+Crafty.init(800, 600, document.getElementById('game'));
 Crafty.e('2D, DOM, Color').attr({x: 0, y: 200, w: 800, h: 200}).color('#007f00');
 Crafty.e('2D, DOM, Color').attr({x: 300, y: 0, w: 200, h: 600}).color('#007f00');
