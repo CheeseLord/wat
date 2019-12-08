df53963e7c97a40a86d534d0061569503c093ec1
Greg Kronmiller <kronmillerg@gmail.com>
2019-12-07 16:12:23 -0800

    Remove a TODO that we already did.

--- src/script/geometry.js
+++ src/script/geometry.js
@@ -14,8 +14,6 @@ import {
 
 // Note: This function is NOT equivalent to getDistance(pos1, pos2) <= 1,
 // because it allows diagonals.
-// TODO: Should we just use L-infinity for distance so it's more consistent
-// with this?
 export function isAdjacent(pos1, pos2) {
     return (Math.abs(pos2.x - pos1.x) <= 1 && Math.abs(pos2.y - pos1.y) <= 1);
 }
