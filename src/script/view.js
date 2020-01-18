374b8137aa72e6003d96029404b60b2190c5b698
Kyle Fox <fox_kyle@yahoo.com>
2020-01-18 18:19:20 -0500

    Make camera work.

--- src/script/view.js
+++ src/script/view.js
@@ -12,9 +12,8 @@ let cameraTarget;
 export function initCamera() {
     // NOTE: To debug the camera when it inevitably breaks, make it visible by
     // adding the "Color" component and calling .color().
-    cameraTarget = Crafty.e("2D, DOM, Multiway, Tween, Color")
+    cameraTarget = Crafty.e("2D, DOM, Multiway, Tween")
             .attr({x: 0, y: 0, z: 100, w: 10, h: 10})
-            .color("red")
             .multiway(800, {
                 UP_ARROW:    -90,
                 DOWN_ARROW:  90,
@@ -26,21 +25,10 @@ export function initCamera() {
 }
 
 export function setFocusOn(character, callback) {
-    var x = character.x + Crafty.viewport.x;
-    var y = character.y + Crafty.viewport.y;
+    var x = character.x + character.w / 2 - cameraTarget.w / 2;
+    var y = character.y + character.h / 2 - cameraTarget.h / 2;
 
-    // TODO Do we want to camera center based on the grid
-    //      or based on the center of characters
-    var midX = character.w / 2;
-    var midY = character.h / 2;
-
-    var centX = ((Crafty.viewport.width + MENU_WIDTH) / 2) /
-        Crafty.viewport._scale;
-    var centY = Crafty.viewport.height / 2 / Crafty.viewport._scale;
-    var newX = x + midX - centX;
-    var newY = y + midY - centY;
-
-    Crafty.one("TweenEnd", callback);
-    cameraTarget.tween({x: newX, y: newY}, ANIM_DUR_CENTER_TURN);
+    cameraTarget.one("TweenEnd", callback);
+    cameraTarget.tween({x: x, y: y}, ANIM_DUR_CENTER_TURN);
 }
 
